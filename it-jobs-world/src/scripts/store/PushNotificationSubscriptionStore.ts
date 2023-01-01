8aa59e9f2426a895e75368e83dbfcaf7d323400c
Dinh Thanh An <an.dinh@minacolor.co.jp>
2023-01-01 20:36:54 +0900

    add log test

--- it-jobs-world/src/scripts/store/PushNotificationSubscriptionStore.ts
+++ it-jobs-world/src/scripts/store/PushNotificationSubscriptionStore.ts
@@ -4,6 +4,7 @@ import { useStorage } from "@vueuse/core";
 export const usePushNotificationSubscriptionStore = defineStore(
   "pushNotificationSubscription",
   () => {
+    console.log("***> call usePushNotificationSubscriptionStore")
     const subscription = useStorage<string | null>("subscription", null);
     return { subscription };
   }
